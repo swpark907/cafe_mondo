842b0cc753ec86c32fea00e850b29d6e7ab69541
swpark907 <76856321+swpark907@users.noreply.github.com>
2022-02-06 01:08:58 +0900

    add more button

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,14 +1,18 @@
 import React from 'react';
 import './Header.css';
+import {images} from '../../constants'
 
 const Header = () => {
   return (
     <div className="app__header app__wrapper section_padding" id="home">
-      <div className="app__header_img"></div>
+      <div className="app__header_img">
+        {/* <img src={images.header1} alt="" /> */}
+      </div>
       <div className="app__header_info">
         <p className="app__header-p">커피로 세상에 향기를 채우다</p>
         <h1 className="app__header-h1">CAFFEMONDO</h1>
         <p className="app__header-p-sub">CAFFEMONDO Coffee & Brunch</p>
+        <button type="button" className="app__header-button">빠져보기</button>
       </div>
     </div>
   );
