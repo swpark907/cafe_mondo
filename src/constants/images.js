bba64e64c71e511f9fda5d682387bda208b50184
swpark907 <76856321+swpark907@users.noreply.github.com>
2022-02-11 23:15:37 +0900

    add constants

---
+++ src/constants/images.js
@@ -0,0 +1,9 @@
+import about1 from '../assets/about_strawberry-latte.png';
+import header1 from '../assets/header1.jpg';
+import header2 from '../assets/header2.jpg';
+import header3 from '../assets/header3.jpg';
+import header4 from '../assets/header4.jpg';
+import header5 from '../assets/header5.png';
+
+
+export default {about1, header1, header2, header3, header4, header5};
