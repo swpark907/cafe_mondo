84d3457a7f47b18eff0c5cb08e070600f6687d04
swpark907 <76856321+swpark907@users.noreply.github.com>
2022-02-16 17:13:24 +0900

    update image, hover effect in Menu section

--- src/components/Home/Menu/Menu.jsx
+++ src/components/Home/Menu/Menu.jsx
@@ -1,11 +1,81 @@
 import React from "react";
 import "./Menu.css";
 import { Link } from "react-router-dom";
+import { images } from "../../../constants";
 
 const Menu = () => {
+
+  const cardActive = (e) => {
+    const card = document.querySelectorAll('.app__menu__card');
+    card.forEach((c) => {
+      c.classList.remove('active');
+    })
+    e.currentTarget.classList.add('active');
+  }
+
+  const cardRemoveActive = (e) => {
+    const card = document.querySelectorAll('.app__menu__card');
+    card.forEach((c) => {
+      c.classList.remove('active');
+    })
+  }
+
   return (
-    <div className="app__wrapper app__menu flex__center">
-      <ul className="app__menu__carousel">
+    <div className="app__wrapper app__menu flex__center section_padding">
+      <ul className="app__menu__cards" onMouseLeave={cardRemoveActive}>
+        <li className="app__menu__card" onMouseOver={cardActive}>
+          <img src={images.menuCoffee} alt="menu-coffee" />
+          <div className="card__label">
+            <h3 className="card__label__name">COFFEE</h3>
+            <p className="card__label__info"></p>
+          </div>
+        </li>
+        <li className="app__menu__card" onMouseOver={cardActive}>
+          <img src={images.menuFrappucino} alt="menu-frappucino" />
+          <div className="card__label">
+            <h3 className="card__label__name">FRAPPUCINO</h3>
+            <p className="card__label__info"></p>
+          </div>
+        </li>
+        <li className="app__menu__card" onMouseOver={cardActive}>
+          <img src={images.menuFreshjuice} alt="menu-freshjuice" />
+          <div className="card__label">
+            <h3 className="card__label__name">FRESH JUICE</h3>
+            <p className="card__label__info">매일 아침 신선한 과일로 갈아드립니다.</p>
+          </div>
+        </li>
+        <li className="app__menu__card" onMouseOver={cardActive}>
+          <img src={images.menuTea} alt="menu-tea" />
+          <div className="card__label">
+            <h3 className="card__label__name">TEA</h3>
+            <p className="card__label__info"></p>
+          </div>
+        </li>
+        <li className="app__menu__card" onMouseOver={cardActive}>
+          <img src={images.menuSmoothie} alt="menu-smoothie" />
+          <div className="card__label">
+            <h3 className="card__label__name">SMOOTHIE</h3>
+            <p className="card__label__info"></p>
+          </div>
+        </li>
+        <li className="app__menu__card" onMouseOver={cardActive}>
+          <img src={images.menuIcecream} alt="menu-icecream" />
+          <div className="card__label">
+            <h3 className="card__label__name">ICE CREAM</h3>
+            <p className="card__label__info"></p>
+          </div>
+        </li>
+      </ul>
+
+
+
+
+
+
+
+
+
+      {/* <ul className="app__menu__carousel">
         <li className="menu__carousel__content flex__center">
           <Link to="#">
             <img src="" alt="" />
@@ -14,7 +84,7 @@ const Menu = () => {
             <p className="content__const">10000원</p>
           </Link>
         </li>
-      </ul>
+      </ul> */}
     </div>
   );
 };
--- src/constants/images.js
+++ src/constants/images.js
@@ -1,9 +1,27 @@
-import about1 from '../assets/about_strawberry-latte.png';
-import header1 from '../assets/header1.jpg';
-import header2 from '../assets/header2.jpg';
-import header3 from '../assets/header3.jpg';
-import header4 from '../assets/header4.jpg';
-import header5 from '../assets/header5.png';
+import about1 from "../assets/about_strawberry-latte.png";
+import header1 from "../assets/header1.jpg";
+import header2 from "../assets/header2.jpg";
+import header3 from "../assets/header3.jpg";
+import header4 from "../assets/header4.jpg";
+import header5 from "../assets/header5.png";
+import menuCoffee from "../assets/menu-coffee.jpg";
+import menuFrappucino from "../assets/menu-frappucino.jpg";
+import menuFreshjuice from "../assets/menu-freshjuice.jpg";
+import menuSmoothie from "../assets/menu-smoothie.jpg";
+import menuTea from "../assets/menu-tea.jpg";
+import menuIcecream from "../assets/menu-icecream.jpg";
 
-
-export default {about1, header1, header2, header3, header4, header5};
+export default {
+  about1,
+  header1,
+  header2,
+  header3,
+  header4,
+  header5,
+  menuCoffee,
+  menuFrappucino,
+  menuFreshjuice,
+  menuSmoothie,
+  menuTea,
+  menuIcecream,
+};
