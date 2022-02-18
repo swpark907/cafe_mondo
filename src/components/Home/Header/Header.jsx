7cb1bfd2c5dd89925dc1159197d9d52db33e404f
swpark907 <76856321+swpark907@users.noreply.github.com>
2022-02-18 16:04:29 +0900

    update display of app__wrapper class

--- src/components/Home/Header/Header.jsx
+++ src/components/Home/Header/Header.jsx
@@ -1,19 +1,19 @@
-import React from 'react';
-import './Header.css';
-import {images} from '../../../constants'
-import { Link } from 'react-router-dom';
+import React from "react";
+import "./Header.css";
+import { images } from "../../../constants";
+import { Link } from "react-router-dom";
 
 const Header = () => {
   return (
     <div className="app__header app__wrapper section_padding" id="home">
       <img src={images.header1} alt="" />
-      <div className="app__header_img">
-      </div>
       <div className="app__header_info">
         <p className="app__header-p">커피로 세상에 향기를 채우다</p>
         <h1 className="app__header-h1">CAFFEMONDO</h1>
         <p className="app__header-p-sub">CAFFEMONDO Coffee & Brunch</p>
-        <button type="button" className="app__header-button"><Link to='/about'>빠져보기</Link></button>
+        <button type="button" className="app__header-button">
+          <Link to="/about">빠져보기</Link>
+        </button>
       </div>
     </div>
   );
