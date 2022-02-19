da514f4331d3eedb193f06577003ed5d3bb6b39e
swpark907 <76856321+swpark907@users.noreply.github.com>
2022-02-19 23:40:00 +0900

    update magazine base UI template

--- src/components/Home/Magazine/Magazine.jsx
+++ src/components/Home/Magazine/Magazine.jsx
@@ -1,9 +1,24 @@
 import React from 'react';
+import './Magazine.css'
 
 const Magazine = () => {
   return (
-    <div>
-      Magazine
+    <div className='app__magazine app__wrapper section_padding flex__center'>
+      <div className="app__magazine__header">
+        <h2 className="app__magazine__header__title">MAGAZINE</h2>
+        <p className="app__magazine__header__subtitle">asdfasdfsasfsaf</p>
+      </div>
+      <div className="app__magazine__columns">
+        <div className="app__magazine__column">
+          asdf
+        </div>
+        <div className="app__magazine__column">
+          asdf
+        </div>
+        <div className="app__magazine__column">
+          asdf
+        </div>
+      </div>
     </div>
   );
 };
