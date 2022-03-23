5f56a37e136516cceff109006bedd7c11e1f1f56
swpark907 <76856321+swpark907@users.noreply.github.com>
2022-03-23 20:48:52 +0900

    update: 유효성 검사

--- src/components/Register/Register.jsx
+++ src/components/Register/Register.jsx
@@ -1,8 +1,8 @@
 import { useEffect, useRef, useState } from "react";
 import "./Register.css";
 
-const ID_REGEX = "";
-const PW_REGEX = "";
+const USER_REGEX = /^[a-z0-9-_]{6,14}$/;
+const PW_REGEX = /^(?=.*[a-z])(?=.*[0-9])(?=.*[!@#$%]).{8,24}$/;
 
 const Register = () => {
   const userRef = useRef();
@@ -18,10 +18,31 @@ const Register = () => {
   const [matchPw, setMatchPw] = useState("");
   const [validMatch, setValidMatch] = useState(false);
   const [matchFocus, setMatchFocus] = useState(false);
+  const [matchLabelMsg, setMatchLabelMsg] = useState("");
 
   useEffect(() => {
     userRef.current.focus();
   }, []);
+
+  useEffect(() => {
+    const result = USER_REGEX.test(user);
+    setValidUser(result);
+  }, [user]);
+
+  useEffect(() => {
+    const result = PW_REGEX.test(pw);
+    setValidPw(result);
+  }, [pw]);
+
+  useEffect(() => {
+    !validPw
+      ? setMatchLabelMsg("먼저 비밀번호를 조건에 맞게 입력해주세요")
+      : setMatchLabelMsg("비밀번호와 다릅니다.");
+  }, [matchPw]);
+
+  useEffect(() => {
+    pw === matchPw && validPw ? setValidMatch(true) : setValidMatch(false);
+  }, [pw, matchPw]);
 
   return (
     <div className="app__register flex__center app__wrapper">
@@ -38,52 +59,76 @@ const Register = () => {
               id="registerId"
               placeholder="영소문자 또는 숫자 6~14자로 입력"
               onChange={(e) => {
-                setUser(e.target.value)
+                setUser(e.target.value);
               }}
             />
-            <label htmlFor="registerId"></label>
+            <label
+              htmlFor="registerId"
+              className={user && !validUser ? "invalid" : "valid"}
+            >
+              아이디가 올바르지 않습니다.
+            </label>
           </div>
         </div>
         <div className="form__list register__form__pw">
           <h3>비밀번호</h3>
           <div className="form__input">
             <input
-              type="text"
+              type="password"
               required
               className="register__input__pw"
               id="registerPw"
               placeholder="영소문자, 숫자 조합으로 8~24자로 입력"
               onChange={(e) => {
-                setPw(e.target.value)
+                setPw(e.target.value);
               }}
             />
-            <label htmlFor="registerPw"></label>
+            <label
+              htmlFor="registerPw"
+              className={pw && !validPw ? "invalid" : "valid"}
+            >
+              비밀번호를 조건에 맞게 입력해 주세요.
+            </label>
           </div>
         </div>
         <div className="form__list register__form__match">
           <h3>비밀번호 확인</h3>
           <div className="form__input">
             <input
-              type="text"
+              type="password"
               required
               className="register__input__match"
               id="registerMatch"
               placeholder="비밀번호를 다시 한번 입력해주세요"
               onChange={(e) => {
-                setMatchPw(e.target.value)
+                setMatchPw(e.target.value);
               }}
             />
-            <label htmlFor="registerMatch"></label>
+            <label
+              htmlFor="registerMatch"
+              className={matchPw && !validMatch ? "invalid" : "valid"}
+            >
+              {matchLabelMsg}
+            </label>
           </div>
         </div>
+
+        <button
+          className="form__submitBtn"
+          disabled={!validUser || !validPw || !validMatch ? true : false}
+        >
+          회원가입
+        </button>
       </form>
       <div className="register__oAuth">
         <button className="oAuth_btn register__google">
           <div className="icon">a</div>
-          구글로 회원가입</button>
+          구글로 회원가입
+        </button>
         <button className="oAuth_btn register__naver">
           <div className="icon">a</div>
-          카카오로 회원가입</button>
+          카카오로 회원가입
+        </button>
         <button className="oAuth_btn register__kakao">
           <div className="icon">a</div>
           네이버 아이디로 회원가입
