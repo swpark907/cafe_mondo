b15b7e8826a50d708468d9d0323b5d65518b4522
swpark907 <76856321+swpark907@users.noreply.github.com>
2022-03-26 21:25:55 +0900

    add PhoneForm

---
+++ src/components/Register/Phone/PhoneForm.jsx
@@ -0,0 +1,57 @@
+import React from "react";
+import "./PhoneForm.css";
+import { useState, useEffect } from 'react';
+
+const Phone = ({ setPhoneNumber }) => {
+  const [phone1, setPhone1] = useState("");
+  const [phone2, setPhone2] = useState("");
+  const [phone3, setPhone3] = useState("");
+
+  const phoneHnadler = () => {
+    let fitNumber = `${phone1}${phone2}${phone3}`
+    setPhoneNumber(fitNumber);
+  };
+
+  useEffect(() => {
+    phoneHnadler();
+  }, [phone1, phone2, phone3]);
+
+  return (
+    <div className="form__list">
+      <h3>연락처</h3>
+      <div className="form__input form__phone">
+        <input
+          className="phone__input"
+          id="phone1"
+          type="text"
+          maxLength={4}
+          onChange={(e) => {
+            setPhone1(e.target.value)
+          }}
+        />
+        &nbsp;-&nbsp;
+        <input
+          className="phone__input"
+          id="phone2"
+          type="text"
+          maxLength={4}
+          onChange={(e) => {
+            setPhone2(e.target.value)
+          }}
+        />
+        &nbsp;-&nbsp;
+        <input
+          className="phone__input"
+          id="phone3"
+          type="text"
+          maxLength={4}
+          onChange={(e) => {
+            setPhone3(e.target.value)
+          }}
+        />
+      </div>
+    </div>
+  );
+};
+
+export default Phone;
