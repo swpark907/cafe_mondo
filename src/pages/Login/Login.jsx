9a02aebeaa1d32594f17ebbeec809f40b642f1b8
swpark907 <76856321+swpark907@users.noreply.github.com>
2022-05-06 21:41:44 +0900

    update: 로그인 페이지 element 생성

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,9 +1,19 @@
+import { useState } from 'react';
 import './Login.css';
 
 const Login = () => {
+
+  const [userId, setUserId] = useState('');
+  const [userPw, setUserPw] = useState('');
+
   return (
-    <div>
-      
+    <div className='app__login flex-center app__wrapper section_padding'>
+      <h2>로그인</h2>
+      <form className='login__form'>
+        <input type="text" placeholder='아이디'/>
+        <input type="text" placeholder='비밀번호'/>
+        <button>로그인</button>
+      </form>
     </div>
   );
 };
