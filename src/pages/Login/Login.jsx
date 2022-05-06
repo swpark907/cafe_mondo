cfaa8822b4578787b2cc9503817d28ee4d81d8f4
swpark907 <76856321+swpark907@users.noreply.github.com>
2022-05-07 06:21:16 +0900

    update: Login 컴포넌트 스타일링

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,18 +1,19 @@
-import { useState } from 'react';
-import './Login.css';
+import { useState } from "react";
+import "./Login.css";
 
 const Login = () => {
-
-  const [userId, setUserId] = useState('');
-  const [userPw, setUserPw] = useState('');
+  const [userId, setUserId] = useState("");
+  const [userPw, setUserPw] = useState("");
 
   return (
-    <div className='app__login flex-center app__wrapper section_padding'>
+    <div className="app__login flex__center app__wrapper section_padding">
       <h2>로그인</h2>
-      <form className='login__form'>
-        <input type="text" placeholder='아이디'/>
-        <input type="text" placeholder='비밀번호'/>
+      <form className="login__form">
+        <div className="id-pw-wrap">
+          <input type="text" placeholder="아이디" />
+          <input type="text" placeholder="비밀번호" />
         <button>로그인</button>
+        </div>
       </form>
     </div>
   );
