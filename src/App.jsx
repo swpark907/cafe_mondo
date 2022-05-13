de202b898fc7db8784728b549e1c61a8c21036c1
swpark907 <76856321+swpark907@users.noreply.github.com>
2022-05-13 23:29:40 +0900

    update: isLogin state 추가

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,19 @@
 import "./App.css";
 import { Route, Routes } from "react-router-dom";
 import {Navbar, Footer } from "./components";
+import { useState } from "react";
 
 import {About, Home, Login, Register} from "./pages/index";
 
 function App() {
+  const [isLogin, setIsLogin] = useState(false);
   return (
     <>
       <Navbar />
       <Routes>
         <Route path="/" exact element={<Home />} />
         <Route path="/register" exact element={<Register />} />
-        <Route path="/login" exact element={<Login />} />
+        <Route path="/login" exact element={<Login setIsLogin={setIsLogin} isLogin={isLogin} />} />
         <Route path="/about" element={<About />} />
       </Routes>
       <Footer />
