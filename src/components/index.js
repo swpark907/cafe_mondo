ced6bcfcc9b0238f35ac81bf57e8414de2e7c25b
swpark907 <76856321+swpark907@users.noreply.github.com>
2022-05-14 23:40:34 +0900

    update: export 경로 추가(Button, SocialButton)

--- src/components/index.js
+++ src/components/index.js
@@ -1,17 +1,18 @@
-export {default as Navbar} from './common/Navbar/Navbar';
+export { default as Navbar } from "./common/Navbar/Navbar";
+export { default as SocialButton } from "./common/SocialButton/SocialButton";
+export { default as Button } from "./common/Button/Button";
+export { default as Footer } from "./common/Footer/Footer";
 
-export {default as Footer} from './common/Footer/Footer';
-export {default as Header} from './pages/Home/Header/Header';
-export {default as About} from './pages/Home/About/About';
-export {default as Menu} from './pages/Home/Menu/Menu';
-export {default as Magazine} from './pages/Home/Magazine/Magazine';
-export {default as Contact} from './pages/Home/Contact/Contact';
+export { default as Header } from "./pages/Home/Header/Header";
+export { default as About } from "./pages/Home/About/About";
+export { default as Menu } from "./pages/Home/Menu/Menu";
+export { default as Magazine } from "./pages/Home/Magazine/Magazine";
+export { default as Contact } from "./pages/Home/Contact/Contact";
 
-
-export {default as AddressForm} from './pages/Register/AddressForm/AddressForm';
-export {default as EmailForm} from './pages/Register/EmailForm/EmailForm'
-export {default as MatchPwForm} from './pages/Register/MatchPwForm/MatchPwForm'
-export {default as PhoneForm} from './pages/Register/PhoneForm/PhoneForm'
-export {default as PwForm} from './pages/Register/PwForm/PwForm'
-export {default as SocialLogin} from './pages/Register/SocialLogin/SocialLogin'
-export {default as UserForm} from './pages/Register/UserForm/UserForm'
+export { default as AddressForm } from "./pages/Register/AddressForm/AddressForm";
+export { default as EmailForm } from "./pages/Register/EmailForm/EmailForm";
+export { default as MatchPwForm } from "./pages/Register/MatchPwForm/MatchPwForm";
+export { default as PhoneForm } from "./pages/Register/PhoneForm/PhoneForm";
+export { default as PwForm } from "./pages/Register/PwForm/PwForm";
+export { default as SocialLogin } from "./pages/Register/SocialLogin/SocialLogin";
+export { default as UserForm } from "./pages/Register/UserForm/UserForm";
